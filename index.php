af144b8cf1e167862f708c19ef40c0fe1f9c53c2
medi_rudiant <medirudiantoni2@gmail.com>
2025-05-09 16:14:23 +0700

    m

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
+<!-- Jawaban No 1 -->
 <?php 
+    // Tidak ditemukan customer_id 4 pada Orders column customer_id.
+    // sehingga pelanggan yang tidak membuat pesanan adalah customer dengan customer_id 4 yaitu | 4           | Delta Corp    | Madrid        |
+?>
 
-// $data = [10, 9, 4, 6];
+<!-- Jawaban No 2 -->
+<?php 
 
 function answer($data, $n){
     $counts = array_count_values($data);
@@ -8,18 +13,21 @@ function answer($data, $n){
 
     foreach($data as $id){
         if($counts[$id] <= $n){
-            return $res[] = $id;
+            $res[] = $id;
         }
     }
 
     return $res;
 }
 
-$data = [5, 10, 8, 4, 6, 9];
-$n = 5;
+$data = [5, 10, 15, 10, 7];
+$n = 1;
 $result = answer($data,$n);
 
-print_r($result);
-
-
 ?>
+
+<p>
+    <?php foreach($result as $item) { ?>
+        <span><?php print($item) ?></span>
+    <?php } ?>
+</p>
